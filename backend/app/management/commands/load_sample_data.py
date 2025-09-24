386bc73ea0659efc4560526452351b02e6f25a5f
tanishdragonemperor <tanigupt333@gmail.com>
2025-09-24 17:08:07 -0500

    Fix Django model field names and data structure mapping - critical database loading fix

--- backend/app/management/commands/load_sample_data.py
+++ backend/app/management/commands/load_sample_data.py
@@ -46,7 +46,7 @@ class Command(BaseCommand):
         
         for team_data in teams_data:
             team, created = Team.objects.get_or_create(
-                teamID=team_data['team_id'],
+                team_id=team_data['team_id'],
                 defaults={'name': team_data['name']}
             )
             if created:
@@ -63,11 +63,18 @@ class Command(BaseCommand):
             players_data = json.load(f)
         
         for player_data in players_data:
+            # Get the team object first
+            try:
+                team = Team.objects.get(team_id=player_data['team_id'])
+            except Team.DoesNotExist:
+                self.stdout.write(f'  Warning: Team with ID {player_data["team_id"]} not found for player {player_data["name"]}')
+                continue
+                
             player, created = Player.objects.get_or_create(
-                playerID=player_data['player_id'],
+                player_id=player_data['player_id'],
                 defaults={
                     'name': player_data['name'],
-                    'teamID': player_data['team_id']
+                    'team': team
                 }
             )
             if created:
@@ -85,89 +92,127 @@ class Command(BaseCommand):
         
         for game_data in games_data:
             game, created = Game.objects.get_or_create(
-                gameID=game_data['id'],
+                game_id=game_data['id'],
                 defaults={
                     'date': datetime.strptime(game_data['date'], '%Y-%m-%d').date()
                 }
             )
             if created:
-                self.stdout.write(f'  Created game: {game.gameID}')
+                self.stdout.write(f'  Created game: {game.game_id}')
             else:
-                self.stdout.write(f'  Game already exists: {game.gameID}')
+                self.stdout.write(f'  Game already exists: {game.game_id}')
 
     def load_shots(self):
-        """Load shots data from games.json"""
+        """Load shots data from players.json"""
         self.stdout.write('Loading shots...')
-        games_path = os.path.join(os.path.dirname(os.path.dirname(os.path.dirname(os.path.dirname(__file__)))), 'raw_data', 'games.json')
+        players_path = os.path.join(os.path.dirname(os.path.dirname(os.path.dirname(os.path.dirname(__file__)))), 'raw_data', 'players.json')
         
-        with open(games_path, 'r') as f:
-            games_data = json.load(f)
+        with open(players_path, 'r') as f:
+            players_data = json.load(f)
         
         shot_count = 0
-        for game_data in games_data:
-            for shot_data in game_data.get('shots', []):
-                shot, created = Shot.objects.get_or_create(
-                    shotID=shot_data['shotID'],
-                    defaults={
-                        'gameID': game_data['gameID'],
-                        'playerID': shot_data['playerID'],
-                        'loc': shot_data['loc'],
-                        'points': shot_data['points']
-                    }
-                )
-                if created:
-                    shot_count += 1
+        for player_data in players_data:
+            try:
+                player = Player.objects.get(player_id=player_data['player_id'])
+                for shot_data in player_data.get('shots', []):
+                    try:
+                        game = Game.objects.get(game_id=shot_data['game_id'])
+                        shot, created = Shot.objects.get_or_create(
+                            shot_id=shot_data['id'],
+                            defaults={
+                                'player': player,
+                                'game': game,
+                                'points': shot_data['points'],
+                                'shooting_foul_drawn': shot_data['shooting_foul_drawn'],
+                                'shot_loc_x': shot_data['shot_loc_x'],
+                                'shot_loc_y': shot_data['shot_loc_y'],
+                                'action_type': shot_data['action_type']
+                            }
+                        )
+                        if created:
+                            shot_count += 1
+                    except Game.DoesNotExist:
+                        self.stdout.write(f'  Warning: Game {shot_data["game_id"]} not found for shot {shot_data["id"]}')
+                        continue
+            except Player.DoesNotExist:
+                self.stdout.write(f'  Warning: Player {player_data["player_id"]} not found for shots')
+                continue
         
         self.stdout.write(f'  Loaded {shot_count} shots')
 
     def load_passes(self):
-        """Load passes data from games.json"""
+        """Load passes data from players.json"""
         self.stdout.write('Loading passes...')
-        games_path = os.path.join(os.path.dirname(os.path.dirname(os.path.dirname(os.path.dirname(__file__)))), 'raw_data', 'games.json')
+        players_path = os.path.join(os.path.dirname(os.path.dirname(os.path.dirname(os.path.dirname(__file__)))), 'raw_data', 'players.json')
         
-        with open(games_path, 'r') as f:
-            games_data = json.load(f)
+        with open(players_path, 'r') as f:
+            players_data = json.load(f)
         
         pass_count = 0
-        for game_data in games_data:
-            for pass_data in game_data.get('passes', []):
-                pass_obj, created = Pass.objects.get_or_create(
-                    passID=pass_data['passID'],
-                    defaults={
-                        'gameID': game_data['gameID'],
-                        'playerID': pass_data['playerID'],
-                        'startLoc': pass_data['startLoc'],
-                        'endLoc': pass_data['endLoc'],
-                        'isCompleted': pass_data['isCompleted'],
-                        'isPotentialAssist': pass_data['isPotentialAssist'],
-                        'isTurnover': pass_data['isTurnover']
-                    }
-                )
-                if created:
-                    pass_count += 1
+        for player_data in players_data:
+            try:
+                player = Player.objects.get(player_id=player_data['player_id'])
+                for pass_data in player_data.get('passes', []):
+                    try:
+                        game = Game.objects.get(game_id=pass_data['game_id'])
+                        pass_obj, created = Pass.objects.get_or_create(
+                            pass_id=pass_data['id'],
+                            defaults={
+                                'player': player,
+                                'game': game,
+                                'completed_pass': pass_data['completed_pass'],
+                                'potential_assist': pass_data['potential_assist'],
+                                'turnover': pass_data['turnover'],
+                                'ball_start_loc_x': pass_data['ball_start_loc_x'],
+                                'ball_start_loc_y': pass_data['ball_start_loc_y'],
+                                'ball_end_loc_x': pass_data['ball_end_loc_x'],
+                                'ball_end_loc_y': pass_data['ball_end_loc_y'],
+                                'action_type': pass_data['action_type']
+                            }
+                        )
+                        if created:
+                            pass_count += 1
+                    except Game.DoesNotExist:
+                        self.stdout.write(f'  Warning: Game {pass_data["game_id"]} not found for pass {pass_data["id"]}')
+                        continue
+            except Player.DoesNotExist:
+                self.stdout.write(f'  Warning: Player {player_data["player_id"]} not found for passes')
+                continue
         
         self.stdout.write(f'  Loaded {pass_count} passes')
 
     def load_turnovers(self):
-        """Load turnovers data from games.json"""
+        """Load turnovers data from players.json"""
         self.stdout.write('Loading turnovers...')
-        games_path = os.path.join(os.path.dirname(os.path.dirname(os.path.dirname(os.path.dirname(__file__)))), 'raw_data', 'games.json')
+        players_path = os.path.join(os.path.dirname(os.path.dirname(os.path.dirname(os.path.dirname(__file__)))), 'raw_data', 'players.json')
         
-        with open(games_path, 'r') as f:
-            games_data = json.load(f)
+        with open(players_path, 'r') as f:
+            players_data = json.load(f)
         
         turnover_count = 0
-        for game_data in games_data:
-            for turnover_data in game_data.get('turnovers', []):
-                turnover, created = Turnover.objects.get_or_create(
-                    turnoverID=turnover_data['turnoverID'],
-                    defaults={
-                        'gameID': game_data['gameID'],
-                        'playerID': turnover_data['playerID'],
-                        'loc': turnover_data['loc']
-                    }
-                )
-                if created:
-                    turnover_count += 1
+        for player_data in players_data:
+            try:
+                player = Player.objects.get(player_id=player_data['player_id'])
+                for turnover_data in player_data.get('turnovers', []):
+                    try:
+                        game = Game.objects.get(game_id=turnover_data['game_id'])
+                        turnover, created = Turnover.objects.get_or_create(
+                            turnover_id=turnover_data['id'],
+                            defaults={
+                                'player': player,
+                                'game': game,
+                                'tov_loc_x': turnover_data['tov_loc_x'],
+                                'tov_loc_y': turnover_data['tov_loc_y'],
+                                'action_type': turnover_data['action_type']
+                            }
+                        )
+                        if created:
+                            turnover_count += 1
+                    except Game.DoesNotExist:
+                        self.stdout.write(f'  Warning: Game {turnover_data["game_id"]} not found for turnover {turnover_data["id"]}')
+                        continue
+            except Player.DoesNotExist:
+                self.stdout.write(f'  Warning: Player {player_data["player_id"]} not found for turnovers')
+                continue
         
         self.stdout.write(f'  Loaded {turnover_count} turnovers')
